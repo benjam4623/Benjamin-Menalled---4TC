b081f9600504b58294709ece32df98f4c2e0a082
benjam4623 <bmenalled@gmail.com>
2025-04-10 12:07:27 -0300

    simulacro examen

---
+++ Assets/heladeria.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heladeria : MonoBehaviour
+{
+    public string GustoHelado;
+    public int CantidadHelado;
+
+
+    float PrecioHelado;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (CantidadHelado < 250 || CantidadHelado > 3000)
+        {
+            Debug.Log("Cantidad de helado no válida");
+            return;
+        }
+
+        if (GustoHelado != "CHO" && GustoHelado != "DDL" && GustoHelado != "FRU")
+        {
+            Debug.Log("Código no válido");
+            return;
+        }
+
+        PrecioHelado = CantidadHelado * (500 / 1000);
+
+        if (GustoHelado == "FRU")
+        {
+            PrecioHelado = PrecioHelado - PrecioHelado * 0.1f;
+        }
+
+        Debug.Log("Debe abonar " + PrecioHelado);
+    }
+
+
+}
+
+// Update is called once per frame
+void Update()
+{
+
+}
+}
